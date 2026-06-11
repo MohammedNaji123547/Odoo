a69781e4c76a73e68963c182d87514008c6f5e12
MohammedNaji123547 <mohamad.latef16@gmail.com>
2026-06-11 19:46:57 +0300

    Add contract_management module scaffold

---
+++ contract_management/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Contract Management',
+    'version': '17.0.1.0.0',
+    'category': 'Legal',
+    'summary': 'Manage company contracts',
+    'author': 'Your Company',
+    'depends': ['base', 'contacts', 'mail'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
