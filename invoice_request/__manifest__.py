fc37972a52d7814f970f201daaa1d88f529e53c6
MohammedNaji123547 <mohamad.latef16@gmail.com>
2026-06-27 09:45:37 +0300

    chore: bump invoice_request version to 17.0.2.0.0 for production

--- invoice_request/__manifest__.py
+++ invoice_request/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Invoice Request',
-    'version': '17.0.1.0.0',
+    'version': '17.0.2.0.0',
     'category': 'Accounting/Invoicing',
     'summary': 'Request vendor invoice creation from Finance',
     'description': '''
